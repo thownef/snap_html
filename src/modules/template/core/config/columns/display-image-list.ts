ba155dad9c0b168aa64f2d1cdef1000e9b9960fa
thownef <vantho3042000@gmail.com>
2025-04-24 10:34:16 +0700

    change radio design

---
+++ src/modules/template/core/config/columns/display-image-list.ts
@@ -0,0 +1,16 @@
+import { Columns } from '@/shared/core/types/columns.type'
+
+export const displayImageList: Columns[] = [
+  {
+    label: '左揃え',
+    value: 'left'
+  },
+  {
+    label: '中央揃え',
+    value: 'center'
+  },
+  {
+    label: '右揃え',
+    value: 'right'
+  }
+]
---
+++ src/modules/template/core/config/columns/display-pc-list.ts
@@ -0,0 +1,6 @@
+import { Columns } from '@/shared/core/types/columns.type'
+
+export const displayPcOptions: Columns[] = [
+  { value: 'left', label: '左揃え' },
+  { value: 'center', label: '中央揃え' }
+]
