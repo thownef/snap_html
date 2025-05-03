c3854b5e974db00ba729f8c2f2559b435da1c321
thownef <vantho3042000@gmail.com>
2025-05-03 20:34:59 +0700

    add template image button

--- src/modules/template/core/config/blocks/template-block-list.ts
+++ src/modules/template/core/config/blocks/template-block-list.ts
@@ -280,7 +280,7 @@ export const templateBlockList: Record<string, Omit<Block, 'id'>> = {
         ]
       },
       {
-        id: 22,
+        id: 2,
         parts: [
           {
             id: 1,
@@ -295,5 +295,243 @@ export const templateBlockList: Record<string, Omit<Block, 'id'>> = {
         ]
       }
     ]
-  }
+  },
+  imageTextButton: {
+    type: 'default',
+    setting: {
+      bottom: 20,
+      left: 20,
+      right: 20,
+      top: 20,
+      columnsInnerPadding: 20,
+      columnMaxWidth: 270,
+      backgroundColor: 'rgb(255, 255, 255, 0)',
+      mobileLayout: MobileLayout.VERTICAL
+    },
+    columns: [
+      {
+        id: 1,
+        parts: [
+          {
+            id: 1,
+            type: 'image',
+            setting: {
+              isMobileFullWidth: true,
+              align: 'center',
+              widthRate: 100
+            },
+            content: 'https://img.bme.jp/img/htmlmail_v3/template-img/dummy_image_v3.png'
+          },
+          {
+            id: 2,
+            type: 'text',
+            content:
+              '<p style="margin: 0px;">テキストを入力してください。</p><p style="margin: 0px;">テキストを入力してください。</p>',
+            setting: {}
+          },
+          {
+            id: 3,
+            type: 'button',
+            setting: {
+              form: FormButton.ROUND,
+              size: SizeButton.MIDDLE,
+              color: 'rgb(255, 255, 255)',
+              backgroundColor: 'rgb(47, 84, 235)',
+              href: ''
+            },
+            content: 'ボタンテキスト'
+          }
+        ]
+      }
+    ]
+  },
+  imageTextButton1: {
+    type: 'multi',
+    setting: {
+      bottom: 20,
+      left: 20,
+      right: 20,
+      top: 20,
+      columnsInnerPadding: 20,
+      columnMaxWidth: 270,
+      backgroundColor: 'rgb(255, 255, 255, 0)',
+      mobileLayout: MobileLayout.VERTICAL
+    },
+    columns: [
+      {
+        id: 1,
+        parts: [
+          {
+            id: 1,
+            type: 'image',
+            setting: {
+              isMobileFullWidth: true,
+              align: 'center',
+              widthRate: 100
+            },
+            content: 'https://img.bme.jp/img/htmlmail_v3/template-img/dummy_image_v3.png'
+          }
+        ]
+      },
+      {
+        id: 2,
+        parts: [
+          {
+            id: 1,
+            type: 'text',
+            content:
+              '<p style="margin: 0px;">テキストを入力してください。</p><p style="margin: 0px;">テキストを入力してください。</p>',
+            setting: {}
+          },
+          {
+            id: 2,
+            type: 'button',
+            setting: {
+              form: FormButton.ROUND,
+              size: SizeButton.MIDDLE,
+              color: 'rgb(255, 255, 255)',
+              backgroundColor: 'rgb(47, 84, 235)',
+              href: ''
+            },
+            content: 'ボタンテキスト'
+          }
+        ]
+      }
+    ]
+  },
+  imageTextButton2: {
+    type: 'multi',
+    setting: {
+      bottom: 20,
+      left: 20,
+      right: 20,
+      top: 20,
+      columnsInnerPadding: 20,
+      columnMaxWidth: 270,
+      backgroundColor: 'rgb(255, 255, 255, 0)',
+      mobileLayout: MobileLayout.VERTICAL
+    },
+    columns: [
+      {
+        id: 1,
+        parts: [
+          {
+            id: 1,
+            type: 'text',
+            content:
+              '<p style="margin: 0px;">テキストを入力してください。</p><p style="margin: 0px;">テキストを入力してください。</p>',
+            setting: {}
+          },
+          {
+            id: 2,
+            type: 'button',
+            setting: {
+              form: FormButton.ROUND,
+              size: SizeButton.MIDDLE,
+              color: 'rgb(255, 255, 255)',
+              backgroundColor: 'rgb(47, 84, 235)',
+              href: ''
+            },
+            content: 'ボタンテキスト'
+          }
+        ]
+      },
+      {
+        id: 2,
+        parts: [
+          {
+            id: 1,
+            type: 'image',
+            setting: {
+              isMobileFullWidth: true,
+              align: 'center',
+              widthRate: 100
+            },
+            content: 'https://img.bme.jp/img/htmlmail_v3/template-img/dummy_image_v3.png'
+          }
+        ]
+      }
+    ]
+  },
+  imageButton: {
+    type: 'default',
+    setting: {
+      bottom: 20,
+      left: 20,
+      right: 20,
+      top: 20,
+      columnsInnerPadding: 20,
+      columnMaxWidth: 270,
+      backgroundColor: 'rgb(255, 255, 255, 0)',
+      mobileLayout: MobileLayout.VERTICAL
+    },
+    columns: [
+      {
+        id: 1,
+        parts: [
+          {
+            id: 1,
+            type: 'image',
+            setting: {
+              isMobileFullWidth: true,
+              align: 'center',
+              widthRate: 100
+            },
+            content: 'https://img.bme.jp/img/htmlmail_v3/template-img/dummy_image_v3.png'
+          },
+          {
+            id: 2,
+            type: 'button',
+            setting: {
+              form: FormButton.ROUND,
+              size: SizeButton.MIDDLE,
+              color: 'rgb(255, 255, 255)',
+              backgroundColor: 'rgb(47, 84, 235)',
+              href: ''
+            },
+            content: 'ボタンテキスト'
+          }
+        ]
+      },
+    ]
+  },
+  textButton: {
+    type: 'default',
+    setting: {
+      bottom: 20,
+      left: 20,
+      right: 20,
+      top: 20,
+      columnsInnerPadding: 20,
+      columnMaxWidth: 270,
+      backgroundColor: 'rgb(255, 255, 255, 0)',
+      mobileLayout: MobileLayout.VERTICAL
+    },
+    columns: [
+      {
+        id: 1,
+        parts: [
+          {
+            id: 1,
+            type: 'text',
+            content:
+              '<p style="margin: 0px;">テキストを入力してください。</p><p style="margin: 0px;">テキストを入力してください。</p>',
+            setting: {}
+          },
+          {
+            id: 2,
+            type: 'button',
+            setting: {
+              form: FormButton.ROUND,
+              size: SizeButton.MIDDLE,
+              color: 'rgb(255, 255, 255)',
+              backgroundColor: 'rgb(47, 84, 235)',
+              href: ''
+            },
+            content: 'ボタンテキスト'
+          }
+        ]
+      }
+    ]
+  },
 }
